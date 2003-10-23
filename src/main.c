3534c97b266df97b9b22175e932bf4957c529494
Bryan Drewery <bryan@shatow.net>
2003-10-23 05:49:07 +0000

    Fixed a commented out unlink()
    git-svn-id: http://svn.shatow.net/wraith/trunk@110 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/main.c
+++ src/main.c
@@ -1077,7 +1077,7 @@ static void gotspawn(char *filename)
     }
   }
   fclose(fp);
-//  unlink(filename);
+  unlink(filename);
 }
 
 static int spawnbot(char *bin, char *nick, char *ip, char *host, char *ipsix, int cloak)
