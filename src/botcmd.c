9bbb6258590ef379f91a17a543c483abd9ca1990
Bryan Drewery <bryan@shatow.net>
2003-11-17 19:02:59 +0000

    * Removed all mod_close() functions * Fixed do_restart to just rehash() * Fixed a bug in remote command system, was sending back idx instead of ridx
    git-svn-id: http://svn.shatow.net/wraith/trunk@194 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/botcmd.c
+++ src/botcmd.c
@@ -1458,20 +1458,6 @@ botcmd_t C_bot[] =
   {NULL,		NULL}
 };
 
-
-void bounce_simul(int idx, char *buf)
-{
-  char rmsg[SGRAB-110];
-
-  if (!buf || !buf[0] || !dcc[idx].simulbot || !dcc[idx].simulbot[0] || idx < 0) {
-    return;
-  }
-
-  snprintf(rmsg, sizeof rmsg, "r-sr %d %s", idx, buf);          /* remote-simul[r]eturn idx buf */
-
-  putbot(dcc[idx].simulbot, rmsg);
-}
-
 void send_remote_simul(int idx, char *bot, char *cmd, char *par)
 {
   char msg[SGRAB-110];
@@ -1522,6 +1508,19 @@ static void bot_rsim(char *botnick, char *code, char *par)
     dcc[idx].user = get_user_by_handle(userlist, nick);
   }
   check_tcl_dcc(cmd, idx, par);
+}
+
+void bounce_simul(int idx, char *buf)
+{
+  char rmsg[SGRAB-110];
+
+  if (!buf || !buf[0] || !dcc[idx].simulbot || !dcc[idx].simulbot[0] || idx < 0) {
+    return;
+  }
+
+  snprintf(rmsg, sizeof rmsg, "r-sr %d %s", dcc[idx].ridx, buf);          /* remote-simul[r]eturn idx buf */
+
+  putbot(dcc[idx].simulbot, rmsg);
 }
 
 #ifdef HUB
