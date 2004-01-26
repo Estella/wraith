e3beb3f1801ca2accd17b345339348553573afdf
Bryan Drewery <bryan@shatow.net>
2004-01-26 22:33:32 +0000

    * Removed share_greet and made always true. * Removed some tcl vars, traces in channels.mod
    git-svn-id: http://svn.shatow.net/wraith/trunk@315 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/userent.c
+++ src/userent.c
@@ -16,7 +16,6 @@ extern Tcl_Interp	*interp;
 extern char		 whois_fields[], botnetnick[];
 extern time_t            now;
 
-int share_greet = 0;		/* Share greeting info			*/
 static struct user_entry_type *entry_type_list;
 
 
@@ -133,9 +132,7 @@ int def_set(struct userrec *u, struct user_entry *e, void *buf)
     e->u.string = NULL;
   }
   if (!noshare && !(u->flags & (USER_BOT | USER_UNSHARED))) {
-    if (e->type != &USERENTRY_INFO || share_greet)
-      shareout(NULL, "c %s %s %s\n", e->type->name, u->handle,
-	       e->u.string ? e->u.string : "");
+    shareout(NULL, "c %s %s %s\n", e->type->name, u->handle, e->u.string ? e->u.string : "");
   }
   return 1;
 }
