8cae4ce51817e23dab5ded265a2ca1d8a61355d3
Bryan Drewery <bryan@shatow.net>
2004-02-24 21:50:32 +0000

    * main() was calling fork() too early.
    git-svn-id: http://svn.shatow.net/wraith/trunk@422 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/main.c
+++ src/main.c
@@ -1496,8 +1496,6 @@ int main(int argc, char **argv)
   init_tcl(argc, argv);
   init_auth();
   init_config();
-  if (backgrd)
-    bg_prepare_split();
   init_botcmd();
   link_statics();
 
@@ -1512,6 +1510,9 @@ int main(int argc, char **argv)
   }
   if (checktrace)
     check_trace_start();
+
+  if (backgrd)		/* fork() after we check for tracing and check the ARGS */
+    bg_prepare_split();
 
 #ifdef HUB
   egg_snprintf(tempdir, sizeof tempdir, "%s/tmp", confdir());
