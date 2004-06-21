7c93688afb54165b77985356d8be0fd01cfb9107
Bryan Drewery <bryan@shatow.net>
2004-06-21 06:27:54 +0000

    * Fixed a compile warning
    git-svn-id: http://svn.shatow.net/wraith/trunk@806 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/dcc.c
+++ src/dcc.c
@@ -1164,7 +1164,12 @@ static void dcc_telnet(int idx, char *buf, int i)
   /* Buffer data received on this socket.  */
   sockoptions(sock, EGG_OPTION_SET, SOCK_BUFFER);
 
-  if (port < 1024 || port > 65535) {
+#if SIZEOF_SHORT == 2
+  if (port < 1024) {
+#else
+   if (port < 1024 || port > 65535) {
+#endif
+
     putlog(LOG_BOTS, "*", DCC_BADSRC, s, port);
     killsock(sock);
     return;
