ac3721e727231eb39d9c88aa0b37d5aca69a3cb2
Bryan Drewery <bryan@shatow.net>
2004-07-06 15:15:11 +0000

    * Changed bits to uint64_t * flag_t is now unsigned long long * Added flag['char'] business
    git-svn-id: http://svn.shatow.net/wraith/trunk@956 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/bits.h
+++ src/bits.h
@@ -3,37 +3,37 @@
 
 #include <sys/types.h>
 
-#define BIT0	(unsigned long) 0x000000001
-#define BIT1	(unsigned long) 0x000000002
-#define BIT2	(unsigned long) 0x000000004
-#define BIT3	(unsigned long) 0x000000008
-#define BIT4	(unsigned long) 0x000000010
-#define BIT5	(unsigned long) 0x000000020
-#define BIT6	(unsigned long) 0x000000040
-#define BIT7	(unsigned long) 0x000000080
-#define BIT8	(unsigned long) 0x000000100
-#define BIT9	(unsigned long) 0x000000200
-#define BIT10	(unsigned long) 0x000000400
-#define BIT11	(unsigned long) 0x000000800
-#define BIT12	(unsigned long) 0x000001000
-#define BIT13	(unsigned long) 0x000002000
-#define BIT14	(unsigned long) 0x000004000
-#define BIT15	(unsigned long) 0x000008000
-#define BIT16	(unsigned long) 0x000010000
-#define BIT17	(unsigned long) 0x000020000
-#define BIT18	(unsigned long) 0x000040000
-#define BIT19	(unsigned long) 0x000080000
-#define BIT20	(unsigned long) 0x000100000
-#define BIT21	(unsigned long) 0x000200000
-#define BIT22	(unsigned long) 0x000400000
-#define BIT23	(unsigned long) 0x000800000
-#define BIT24	(unsigned long) 0x001000000
-#define BIT25	(unsigned long) 0x002000000
-#define BIT26	(unsigned long) 0x004000000
-#define BIT27	(unsigned long) 0x008000000
-#define BIT28	(unsigned long) 0x010000000
-#define BIT29	(unsigned long) 0x020000000
-#define BIT30	(unsigned long) 0x040000000
-#define BIT31	(unsigned long) 0x080000000
+#define BIT0	(uint64_t) 0x000000001
+#define BIT1	(uint64_t) 0x000000002
+#define BIT2	(uint64_t) 0x000000004
+#define BIT3	(uint64_t) 0x000000008
+#define BIT4	(uint64_t) 0x000000010
+#define BIT5	(uint64_t) 0x000000020
+#define BIT6	(uint64_t) 0x000000040
+#define BIT7	(uint64_t) 0x000000080
+#define BIT8	(uint64_t) 0x000000100
+#define BIT9	(uint64_t) 0x000000200
+#define BIT10	(uint64_t) 0x000000400
+#define BIT11	(uint64_t) 0x000000800
+#define BIT12	(uint64_t) 0x000001000
+#define BIT13	(uint64_t) 0x000002000
+#define BIT14	(uint64_t) 0x000004000
+#define BIT15	(uint64_t) 0x000008000
+#define BIT16	(uint64_t) 0x000010000
+#define BIT17	(uint64_t) 0x000020000
+#define BIT18	(uint64_t) 0x000040000
+#define BIT19	(uint64_t) 0x000080000
+#define BIT20	(uint64_t) 0x000100000
+#define BIT21	(uint64_t) 0x000200000
+#define BIT22	(uint64_t) 0x000400000
+#define BIT23	(uint64_t) 0x000800000
+#define BIT24	(uint64_t) 0x001000000
+#define BIT25	(uint64_t) 0x002000000
+#define BIT26	(uint64_t) 0x004000000
+#define BIT27	(uint64_t) 0x008000000
+#define BIT28	(uint64_t) 0x010000000
+#define BIT29	(uint64_t) 0x020000000
+#define BIT30	(uint64_t) 0x040000000
+#define BIT31	(uint64_t) 0x080000000
 
 #endif /* !_BITS_H */
