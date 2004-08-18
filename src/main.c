62de1ee055af6eb83dfab5b118a19dc42c8e9bb8
Bryan Drewery <bryan@shatow.net>
2004-08-18 19:19:04 +0000

    * No longer displaying SALTS on ./bin -v
    git-svn-id: http://svn.shatow.net/wraith/trunk@1143 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@
 #include "misc.h"
 #include "thread.h"
 #include "settings.h"
-#include "salt.h"
 #include "misc_file.h"
 #include "net.h"
 #include "users.h"
@@ -340,7 +339,6 @@ static void dtx_arg(int argc, char *argv[])
 
         egg_strftime(date, sizeof date, "%c %Z", gmtime(&buildts));
 	printf("%s\nBuild Date: %s (%lu)\n", version, date, buildts);
-        printf("SALTS\nfiles: %s\nbotlink: %s\n", SALT1, SALT2);
 	exit(0);
       }
 #ifdef LEAF
