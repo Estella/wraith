dd1f4613ddcddbb8065676a76cd06d5b8114ed8e
Bryan Drewery <bryan@shatow.net>
2004-08-30 18:52:40 +0000

    * Don't auto-op users returning from split
    git-svn-id: http://svn.shatow.net/wraith/trunk@1184 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/mod/irc.mod/chan.c
+++ src/mod/irc.mod/chan.c
@@ -2127,6 +2127,8 @@ static int gotjoin(char *from, char *chname)
       chan->status &= ~CHAN_PEND;
       reset_chan_info(chan);
     } else {
+      int splitjoin = 0;
+
       m = ismember(chan, nick);
       if (m && m->split && !egg_strcasecmp(m->userhost, uhost)) {
 	chan = findchan(chname);
@@ -2142,6 +2144,7 @@ static int gotjoin(char *from, char *chname)
 
 	/* The tcl binding might have deleted the current user. Recheck. */
 	u = get_user_by_host(from);
+        splitjoin++;
 	m->split = 0;
 	m->last = now;
 	m->delay = 0L;
@@ -2246,7 +2249,7 @@ static int gotjoin(char *from, char *chname)
 	    m->flags |= SENTKICK;
 	  }
 	}
-        if (!chan_hasop(m) && dovoice(chan) && chk_autoop(fr, chan)) {
+        if (!splitjoin && !chan_hasop(m) && dovoice(chan) && chk_autoop(fr, chan)) {
           do_op(m->nick, chan, 1, 0);
         }
       }
