fbfbdebae8791971b22ce059614c09594b0f1d8e
Bryan Drewery <bryan@shatow.net>
2004-10-04 19:20:53 +0000

    *** empty log message ***
    git-svn-id: http://svn.shatow.net/wraith/trunk@1255 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/cmds.c
+++ src/cmds.c
@@ -1744,7 +1744,7 @@ static void cmd_comment(struct userrec *u, int idx, char *par)
     dprintf(idx, "No such user!\n");
     return;
   }
-  if (whois_access(dcc[idx].user, u1))
+  if (whois_access(dcc[idx].user, u1)) {
     dprintf(idx, "You can't change comment on higher level users.\n");
     return;
   }
