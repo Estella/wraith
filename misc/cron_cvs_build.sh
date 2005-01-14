0eb4156459f31046ab747fc7a64f2efa74bfde10
Bryan Drewery <bryan@shatow.net>
2005-01-14 18:50:33 +0000

    * Updated script to be standalone
    git-svn-id: http://svn.shatow.net/wraith/trunk@1541 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- misc/cron_cvs_build.sh
+++ misc/cron_cvs_build.sh
@@ -1,18 +1,26 @@
 #! /bin/sh
 
-path="${HOME}/wraith/misc/cron_cvs_build.sh"
+path="${HOME}/cron_cvs_build.sh"
 #path="${PWD}/`basename $0`"
+PRIVATE_ROOT=":pserver:changelog:S0meP4ass:@box.shatow.net:/cvs"
 
 if test $# -lt 1; then
  #Crontab this script.
- HOURLY=10
  TMPFILE=`mktemp /tmp/cron.XXXXXX` || exit 1
  crontab -l | grep -v "${path}" > ${TMPFILE}
  echo "* 0 * * * ${path} 1 > /dev/null 2>&1" >> ${TMPFILE}
  crontab ${TMPFILE}
  rm ${TMPFILE}
 else
- cd ${HOME}/wraith
- cvs update
+ cd ${HOME}
+ mkdir tmp
+ cd ${HOME}/tmp/
+ cvs -d $PRIVATE_ROOT login
+ cvs -z9 -d $PRIVATE_ROOT checkout wraith
+ cd ${HOME}/tmp/wraith/
+# export PWD="${HOME}/wraith/"
  ./build -C -n -s lordares@endurance.quadspeedi.net:public_html/nightly/ all
+ cd ${HOME}/tmp/
+ rm -rf wraith
+ cvs -d $PRIVATE_ROOT logout
 fi
