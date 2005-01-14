28258669f496f3c3beb22e39da98b3e2abb972a8
Bryan Drewery <bryan@shatow.net>
2005-01-14 23:57:43 +0000

    * Make the nightly cron script auto update
    git-svn-id: http://svn.shatow.net/wraith/trunk@1543 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- misc/cron_cvs_build.sh
+++ misc/cron_cvs_build.sh
@@ -11,16 +11,23 @@ if test $# -lt 1; then
  echo "* 0 * * * ${path} 1 > /dev/null 2>&1" >> ${TMPFILE}
  crontab ${TMPFILE}
  rm ${TMPFILE}
-else
- cd ${HOME}
- mkdir tmp
- cd ${HOME}/tmp/
- cvs -d $PRIVATE_ROOT login
- cvs -z9 -d $PRIVATE_ROOT checkout wraith
- cd ${HOME}/tmp/wraith/
-# export PWD="${HOME}/wraith/"
- ./build -C -n -s lordares@endurance.quadspeedi.net:public_html/nightly/ all
- cd ${HOME}/tmp/
- rm -rf wraith
- cvs -d $PRIVATE_ROOT logout
+ 
+ exit 0
 fi
+
+cd ${HOME}
+mkdir tmp
+cd ${HOME}/tmp/
+cvs -d $PRIVATE_ROOT login
+cvs -z9 -d $PRIVATE_ROOT checkout wraith
+cd ${HOME}/tmp/wraith/
+# export PWD="${HOME}/wraith/"
+./build -C -n -s lordares@endurance.quadspeedi.net:public_html/nightly/ all
+cd ${HOME}/tmp/
+cp ${HOME}/tmp/wraith/misc/cron_cvs_build.sh ${HOME}/tmp/cron_cvs_build.sh
+rm -rf ${HOME}/tmp/wraith
+cvs -d $PRIVATE_ROOT logout
+
+mv ${HOME}/tmp/cron_cvs_build.sh ${HOME}/cron_cvs_build.sh
+
+
