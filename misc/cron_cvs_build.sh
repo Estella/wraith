99b9dc333e86cc79c6246cec0a33145ae686a970
Bryan Drewery <bryan@shatow.net>
2005-01-16 09:03:51 +0000

    *** empty log message ***
    git-svn-id: http://svn.shatow.net/wraith/trunk@1546 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- misc/cron_cvs_build.sh
+++ misc/cron_cvs_build.sh
@@ -24,10 +24,10 @@ cd ${HOME}/tmp/wraith/
 # export PWD="${HOME}/wraith/"
 ./build -C -n -s lordares@endurance.quadspeedi.net:public_html/nightly/ all
 cd ${HOME}/tmp/
-cp ${HOME}/tmp/wraith/misc/cron_cvs_build.sh ${HOME}/tmp/cron_cvs_build.sh
+cp -f ${HOME}/tmp/wraith/misc/cron_cvs_build.sh ${HOME}/tmp/cron_cvs_build.sh
 rm -rf ${HOME}/tmp/wraith
 cvs -d $PRIVATE_ROOT logout
 
-mv ${HOME}/tmp/cron_cvs_build.sh ${HOME}/cron_cvs_build.sh
+mv -f ${HOME}/tmp/cron_cvs_build.sh ${HOME}/cron_cvs_build.sh
 
 
