83c08f6f571ab8ce57958d335a200f25cfc9c0f0
Bryan Drewery <bryan@shatow.net>
2005-02-07 05:34:05 +0000

    * Fixed chanset limit not being set every 2 minutes.
    git-svn-id: http://svn.shatow.net/wraith/trunk@1697 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/mod/irc.mod/chan.c
+++ src/mod/irc.mod/chan.c
@@ -1355,6 +1355,7 @@ static int got341(char *from, char *msg)
         cache->timeval = now;
         notice_invite(chan, cache->handle[0] ? cache->handle : NULL, nick, cache->uhost, cchan->op);
 
+//        cache_del_chan_parm(cache, cache->cchan);
         break;
       }
     }
