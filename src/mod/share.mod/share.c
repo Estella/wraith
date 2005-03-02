04e80aaf635852903e03abef95c49437e1ee5c19
Bryan Drewery <bryan@shatow.net>
2005-03-02 21:49:24 +0000

    * Fixed bots not rechecking channel for invalid users after a .-chrec
    git-svn-id: http://svn.shatow.net/wraith/trunk@1902 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/mod/share.mod/share.c
+++ src/mod/share.mod/share.c
@@ -351,6 +351,9 @@ share_mns_chrec(int idx, char *par)
       noshare = 0;
       if (conf.bot->hub)
         putlog(LOG_CMDS, "@", "%s: -chrec %s %s", dcc[idx].nick, user, par);
+      else
+        recheck_channel(chan, 0);
+
     }
   }
 }
