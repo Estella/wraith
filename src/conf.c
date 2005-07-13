c79dc2f882117d521ce045195b8bafb02608197d
Bryan Drewery <bryan@shatow.net>
2005-07-13 17:07:08 +0000

    *** empty log message ***
    git-svn-id: http://svn.shatow.net/wraith/trunk@2243 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/conf.c
+++ src/conf.c
@@ -356,7 +356,7 @@ init_conf()
   char *p = strrchr(binname, '/');
 
   p++;
-  if (strncmp(p, "wraith.", 7))
+  if (strncmp(p, "wraith.", 7) && strchr(p, '-'))
     conf.binname = strdup(p);
   else
     conf.binname = strdup("wraith");
