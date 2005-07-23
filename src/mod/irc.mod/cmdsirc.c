dd87d60a6c38f2753e330c736afa61226d469063
Bryan Drewery <bryan@shatow.net>
2005-07-23 02:03:05 +0000

    * Fixed many channel cmds blocking access due to +botbitch. (#141)
    git-svn-id: http://svn.shatow.net/wraith/trunk@2323 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/mod/irc.mod/cmdsirc.c
+++ src/mod/irc.mod/cmdsirc.c
@@ -227,7 +227,7 @@ static void cmd_kickban(int idx, char *par)
       dprintf(idx, "No such channel.\n");
       return;
     }
-    else if (!chk_op(user, chan)) {
+    else if (!real_chk_op(user, chan, 0)) {
       if (all) goto next;
       dprintf(idx, "You don't have access to %s\n", chan->dname);
       return;
@@ -828,7 +828,7 @@ static void cmd_deop(int idx, char *par)
       if (all) goto next;
       dprintf(idx, "No such channel.\n");
     }
-    else if (!chk_op(user, chan)) {
+    else if (!real_chk_op(user, chan, 0)) {
       if (all) goto next;
       dprintf(idx, "You don't have access to deop on %s\n", chan->dname);
       return;
@@ -930,7 +930,7 @@ static void cmd_kick(int idx, char *par)
       dprintf(idx, "No such channel.\n");
       return;
     }
-    else if (!chk_op(user, chan)) {
+    else if (!real_chk_op(user, chan, 0)) {
       if (all) goto next;
       dprintf(idx, "You don't have access to kick on %s\n", chan->dname);
       return;
@@ -999,7 +999,7 @@ static void cmd_getkey(int idx, char *par)
     dprintf(idx, "No such channel.\n");
     return;
   }
-  else if (!chk_op(user, chan)) {
+  else if (!real_chk_op(user, chan, 0)) {
     dprintf(idx, "You don't have access for %s\n", chan->dname);
     return;
   }
@@ -1222,7 +1222,7 @@ static void do_invite(int idx, char *par, bool op)
       if (all) goto next;
       dprintf(idx, "No such channel.\n");
     }
-    else if (!chk_op(user, chan)) {
+    else if (!real_chk_op(user, chan, 0)) {
       if (all) goto next;
       dprintf(idx, "You don't have access to invite to %s\n", chan->dname);
       return;
