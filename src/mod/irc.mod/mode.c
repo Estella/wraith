967787a213f0a40c5a9436d875b0daf1e5df626e
Bryan Drewery <bryan@shatow.net>
2006-01-15 08:30:09 +0000

    * Ported [2677] to 1.2.9 (Segfault from CHANFIX/server modes (fixes #25))
    git-svn-id: http://svn.shatow.net/wraith/branches/1_2_9@2678 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/mod/irc.mod/mode.c
+++ src/mod/irc.mod/mode.c
@@ -1004,7 +1004,8 @@ gotmode(char *from, char *msg)
           dprintf(DP_MODE, "WHO %s\n", nick);
           return 0;
         }
-      }
+      } else
+        nick = splitnick(&from);
 
       chg = newsplit(&msg);
       reversing = 0;
