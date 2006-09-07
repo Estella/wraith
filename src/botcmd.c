fc5e32efd1c8785a069b208f0a437ee2c30ea1ef
Bryan Drewery <bryan@shatow.net>
2006-09-07 16:16:18 +0000

    * Port [3000] to 1.2.11  * Hub nicks no longer show on leaf bots for partyline chat.
    git-svn-id: http://svn.shatow.net/wraith/branches/1.2.11@3002 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/botcmd.c
+++ src/botcmd.c
@@ -66,7 +66,15 @@ static void bot_chan2(int idx, char *msg)
   }
   p = strchr(from, '@');
   if (p) {
-    simple_snprintf(TBUF, sizeof(TBUF), "<%s> %s", from, msg);
+    if (!conf.bot->hub) { /* Need to strip out the hub nick */
+      char *q = NULL, newfrom[HANDLEN + 9 + 1]; /* HANDLEN@[botnet] */
+   
+      strlcpy(newfrom, from, sizeof(newfrom));
+      q = strchr(newfrom, '@');
+      *q = 0;
+      simple_snprintf(TBUF, sizeof(TBUF), "<%s@[botnet]> %s", newfrom, msg);
+    } else
+      simple_snprintf(TBUF, sizeof(TBUF), "<%s> %s", from, msg);
     *p = 0;
     if (!partyidle(p + 1, from)) {
       *p = '@';
@@ -87,11 +95,6 @@ static void bot_chan2(int idx, char *msg)
     /* Send to new version bots */
     if (i >= 0)
       botnet_send_chan(idx, from, NULL, chan, msg);
-/*    if (strchr(from, '@') != NULL)
-      check_bind_chat(from, chan, msg);
-    else
-      check_bind_bcst(from, chan, msg);
-*/
   }
 }
 
