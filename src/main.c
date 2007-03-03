42c035242c2a7a2b9037eb3ea30a6b93fd9e3b33
Bryan Drewery <bryan@shatow.net>
2007-03-03 07:18:22 +0000

    * Port [3218] to 1.2.14  * All bots are now rehashed after editing binary - nick casing, ip changes are taken into effect.  * Bots now use the shell username instead of botnick when ident does not work.  * Move a lot of the rehash logic into confedit()  * Add origbotnick to handle the original -B nick, as origbotname is the preferred nick on irc and is changed with .botset 'nick'
    git-svn-id: http://svn.shatow.net/wraith/branches/1.2.14@3219 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/main.c
+++ src/main.c
@@ -315,6 +315,7 @@ static void dtx_arg(int argc, char *argv[])
       case 'B':
         used_B = 1;
         strlcpy(origbotname, optarg, HANDLEN + 1);
+        strlcpy(origbotnick, optarg, HANDLEN + 1);
         break;
       case 'H':
         printf("SHA1 (%s): %s\n", optarg, SHA1(optarg));
@@ -574,7 +575,7 @@ static void startup_checks(int hack) {
 
   if (can_stat(cfile))
     readconf(cfile, 0);	/* will read into &conf struct */
-  conf_checkpids();
+  conf_checkpids(conf.bots);
 #endif /* CYGWIN_HACKS */
 
 #ifndef CYGWIN_HACKS
@@ -604,7 +605,7 @@ static void startup_checks(int hack) {
     if (do_killbot[0]) {
       const char *what = (kill_sig == SIGKILL ? "kill" : "restart");
 
-      if (conf_killbot(do_killbot, NULL, kill_sig) == 0)
+      if (conf_killbot(conf.bots, do_killbot, NULL, kill_sig) == 0)
         printf("'%s' successfully %sed.\n", do_killbot, what);
       else {
         printf("Error %sing '%s'\n", what, do_killbot);
@@ -628,7 +629,7 @@ static void startup_checks(int hack) {
       if (!conf.bots || !conf.bots->nick)     /* no bots ! */
         werr(ERR_NOBOTS);
 
-      spawnbots();
+      spawnbots(conf.bots);
       exit(0); /* our job is done! */
     }
   }
@@ -777,7 +778,7 @@ printf("out: %s\n", out);
   console_init();
   chanprog();
 
-  strcpy(botuser, origbotname);
+  strcpy(botuser, conf.username ? conf.username : origbotname);
 
   if (!conf.bot->hub && conf.bot->localhub)
     sdprintf("I am localhub (%s)", conf.bot->nick);
