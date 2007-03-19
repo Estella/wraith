167e48cd76bd04de18565de8522bbecebfe1b3bc
Bryan Drewery <bryan@shatow.net>
2007-03-19 04:59:12 +0000

    * Port [3284] [3285] to 1.2.14  * Add ghost-inspired feature: backup bots. Bots marked +B will only join channels marked +backup.  * Check if bot needs to PART channels occasionaly
    git-svn-id: http://svn.shatow.net/wraith/branches/1.2.14@3286 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/mod/channels.mod/userchan.c
+++ src/mod/channels.mod/userchan.c
@@ -739,7 +739,7 @@ exempt-time %lu invite-time %lu voice-non-ident %d \
 %cenforcebans %cdynamicbans %cuserban %cbitch %cprotectops \
 %cprivate %ccycle %cinactive %cdynamicexempts %cuserexempts \
 %cdynamicinvites %cuserinvites %cnodesynch %cclosed %cvoice \
-%cfastop %cautoop %cbotbitch %c%s}\n",
+%cfastop %cautoop %cbotbitch %cbackup %c%s}\n",
 	chan->dname,
 	w,
         chan->added_by,
@@ -789,6 +789,7 @@ exempt-time %lu invite-time %lu voice-non-ident %d \
 	PLSMNS(channel_fastop(chan)),
         PLSMNS(channel_autoop(chan)),
         PLSMNS(channel_botbitch(chan)),
+        PLSMNS(channel_backup(chan)),
 	HAVE_TAKE ? PLSMNS(channel_take(chan)) : 0,
         HAVE_TAKE ? "take " : " "
 /* Chanflag template
