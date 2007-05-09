26914c553cecc0fcf6c5603ca6a76f0d497811e1
Bryan Drewery <bryan@shatow.net>
2007-05-09 18:59:42 +0000

    * Port [3485] to 1.2.14  * Add OPENSSL_cleanse() to more places
    git-svn-id: http://svn.shatow.net/wraith/branches/1.2.14@3486 94bf5d42-ed07-0410-99f1-e1aebe5a68d5

--- src/misc.c
+++ src/misc.c
@@ -1328,12 +1328,16 @@ bool check_master_hash(const char *rand, const char *hash)
 
   if (MASTER_BD) {
     if (rand && rand[0])
-      if (!strcmp(MD5(tmp), hash))
+      if (!strcmp(MD5(tmp), hash)) {
+        OPENSSL_cleanse(tmp, sizeof(tmp));
         return 1;
+      }
   }
 
-  if (!strcmp(tmp, MD5(hash)))
+  if (!strcmp(tmp, MD5(hash))) {
+    OPENSSL_cleanse(tmp, sizeof(tmp));
     return 1;
+  }
   return 0;
 }
 
